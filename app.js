e1945ebb3820ad403f14c73303ac9c37760e0166
Brandon <mbrandon@labs.neit.edu>
2019-01-17 14:55:33 -0500

    skeleton

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello");
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.use('/', router);
+app.listen(process.env.port || 3000);
+console.log("Running on port 3000");
